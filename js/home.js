76f6af93baca5e931445e4c1ae82d2bd6a7bc234
trieuledinh <tuongthanhvinhcuu@gmail.com>
2017-06-29 17:21:36 +0700

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,10 +1,35 @@
-$(document).ready(function(){
-      //page
-      $('.row').load('services.html');
+$(function () {
+    var load = function (url) {
+        $.get(url).done(function (data) {
+            $("#content").html(data);
+        })
+    };
 
-      $('.nav_link').click(function(){
-        var page = $(this).attr('href');
-        $('.row').load(page);
-        return false;
-      });
+    $(document).on('click', 'a', function (e) {
+        e.preventDefault();
+
+        var $this = $(this),
+            url = $this.attr("href"),
+            title = $this.text();
+
+        history.pushState({
+            url: url,
+            title: title
+        }, title, url);
+
+        document.title = title;
+
+        load(url);
     });
+
+    $(window).on('popstate', function (e) {
+        var state = e.originalEvent.state;
+        if (state !== null) {
+            document.title = state.title;
+            load(state.url);
+        } else {
+            document.title = 'World Regions';
+            $("#content").empty();
+        }
+    });
+});
