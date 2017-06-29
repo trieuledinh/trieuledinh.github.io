7842e7eb6fa332d0f603bf855105f6d05162376f
trieuledinh <tuongthanhvinhcuu@gmail.com>
2017-06-29 17:30:43 +0700

    Update home.js

--- js/home.js
+++ js/home.js
@@ -33,3 +33,27 @@ $(function () {
         }
     });
 });
+
+
+
+// jQuery
+$(window).on('popstate', function (e) {
+    var state = e.originalEvent.state;
+    if (state !== null) {
+        //load content with ajax
+    }
+});
+
+// Vanilla javascript
+window.addEventListener('popstate', function (e) {
+    var state = e.state;
+    if (state !== null) {
+        //load content with ajax
+    }
+});
+
+history.pushState(null, null, 'new-page.html');
+// or
+history.pushState({url: 'new-page.html'}, 'New Page');
+// or
+history.pushState({url: 'new-page.html', page: 2}, 'New Page', 'new-page.html');
